a6f68e3f0a5f32491fd40dba0c78c208525c997b
nadashaban10 <nadashaban10@github.com>
2024-07-11 12:01:17 +0300

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,22 @@
 #!/usr/bin/node
+
 const request = require('request');
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
-// Function to fetch and print character names by movie ID
-function printStarWarsCharacters(movieId) {
-  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-  // Fetch the movie details
-  request(apiUrl, { json: true }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    if (!body || !body.characters) {
-      console.log('No characters found for this movie.');
-      return;
-    }
-
-    // Iterate through the characters list
-    body.characters.forEach((characterUrl) => {
-      // Fetch each character's details
-      request(characterUrl, { json: true }, (charErr, charRes, charBody) => {
-        if (charErr) { return console.log(charErr); }
-        // Print the character's name
-        console.log(charBody.name);
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const characterId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(characterId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
       });
     });
-  });
-}
-
-// Get the movie ID from the first positional argument
-const movieId = process.argv[2];
-
-// Validate the movie ID
-if (!movieId) {
-  console.log('Please provide a Movie ID as the first positional argument.');
-} else {
-  printStarWarsCharacters(movieId);
-}
+  }
+});
